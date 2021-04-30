75617486887a623329869656e524491f597752c1
Alberto Roldán <a-f-r@hotmail.es>
2021-04-30 16:16:46 +0200

    Calculate sum of both inputs and clear input fields when clicking on the button

--- index.js
+++ index.js
@@ -2,7 +2,28 @@
 
 const btnForm = document.querySelector(".btn");
 
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
+const input3 = document.querySelector(".input3");
+
+const twenty = document.querySelector(".twenty");
+
 btnForm.addEventListener("click", function (e) {
+  //Prevent form from submitting
   e.preventDefault();
-  console.log("clicked");
+
+  //Get task
+  const task = input1.value;
+
+  //Calculate sum of both inputs
+  const sum = Number(input2.value) + Number(input3.value);
+
+  console.log(sum, task);
+
+  // Clear input fields
+  input1.value = input2.value = "";
+
+  //Create element
+  const html = `<h1>${task}</h1>`;
+  twenty.insertAdjacentHTML("afterbegin", html);
 });
