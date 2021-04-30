79e2d7b7e00cb5b251b41b5a93862524dcb299ea
Alberto Roldán <a-f-r@hotmail.es>
2021-04-30 19:31:15 +0200

    Show alert if there's no task when submitting

--- index.js
+++ index.js
@@ -29,20 +29,24 @@ btnForm.addEventListener("click", function () {
   //Calculate sum of both inputs
   const sum = Number(input2.value) + Number(input3.value);
 
-  // Clear input fields
-  input1.value = input2.value = input3.value = "";
+  //Show error message if there's no task
+  if (input1.value === "") {
+    alert("Please fill out a task");
+  } else {
+    // Clear input fields
+    input1.value = input2.value = input3.value = "";
+    //Create element
+    const html = `
+      <div class="task">
+          <h3 class="tasks">${task}</h3>
+          <button class="btn2">
+              <i class="fa fa-times" aria-hidden="true"></i>
+          </button>
+      </div>`;
 
-  //Create element
-  const html = `
-    <div class="task">
-        <h3 class="tasks">${task}</h3>
-        <button class="btn2">
-            <i class="fa fa-times" aria-hidden="true"></i>
-        </button>
-    </div>`;
-
-  //Display task in their respective div
-  document.querySelector(`.div${sum}`).insertAdjacentHTML("afterbegin", html);
+    //Display task in their respective div
+    document.querySelector(`.div${sum}`).insertAdjacentHTML("afterbegin", html);
+  }
 
   //Delete task when clicking on btn2
   document.querySelector(".btn2").addEventListener("click", function (e) {
