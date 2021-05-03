a39e29e5b0b48a897d46aff3bfedec23c158faeb
Alberto Roldán <a-f-r@hotmail.es>
2021-05-03 17:56:14 +0200

    Apply fade out effect correctly and place removetask function in the global scope

--- index.js
+++ index.js
@@ -31,34 +31,42 @@ btnForm.addEventListener("click", function () {
     alert("Please fill out every field");
   }
 
-  //Display tasks if values are greate than 0
-
+  //Display task if values are greate than 0
   if (input2.value > 0 && input3.value > 0) {
     // Clear input fields
     input1.value = input2.value = input3.value = "";
     //Create element
     const html = `
-    <li>
-    
-    <div class="task">
-        <h3>${task}</h3>
-        <button class="btn2">
-            <i class="fa fa-times" aria-hidden="true"></i>
-        </button>
-    </div>
+    <li class="fade-out">
+      <div class="task">
+          <h3>${task}</h3>
+          <button class="btn2">
+              <i class="fa fa-times" aria-hidden="true"></i>
+          </button>
+      </div>
     </li>
       `;
 
     //Display task in their respective div
     document.querySelector(`.div${sum}`).insertAdjacentHTML("beforeend", html);
   }
-
-  //Delete task when clicking on btn2
-  document.querySelector(".btn2").addEventListener("click", function (e) {
-    const removeTarget = e.target.parentNode.parentNode;
-    removeTarget.style.opacity = "0";
-    setTimeout(function () {
-      removeTarget.parentNode.remove();
-    }, 1000);
-  });
 });
+
+//
+
+//Delete task when clicking on btn2
+const removeTask = function (e) {
+  const element = e.target;
+  if (
+    element.classList.contains("btn2") ||
+    element.classList.contains("fa-times")
+  ) {
+    //Fade out effect
+    e.target.closest(".fade-out").style.opacity = "0";
+    setTimeout(function () {
+      element.closest("li").remove();
+    }, 1000);
+  }
+};
+
+document.addEventListener("click", removeTask);
